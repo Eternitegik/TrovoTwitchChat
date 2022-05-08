36a021c8271a8c99d1ab92072beeff273725ab14
Eternitegik <16bdidima@gmail.com>
2022-05-08 16:12:18 +0300

    Open URL link

--- ind.js
+++ ind.js
@@ -4,6 +4,9 @@ window.onload = function(){
     var urlSearch = document.location.search;
     var urlPathname = document.location.pathname;
     
+    var userTrovo = '';
+    var userTwitch = '';
+    
     console.log("urlProtocol:" + urlProtocol);
     console.log("urlHost:" + urlHost);
     console.log("urlSearch:" + urlSearch);
@@ -15,41 +18,54 @@ window.onload = function(){
         console.log('----------------------2');
         urlSearch = urlSearch.substring(1);
         urlSearch = urlSearch.split("&");
-        var urlSearchA = urlSearch[0];
-        urlSearchA = urlSearchA.split("=")[1];
-        var urlSearchB = urlSearch[1];
-        urlSearchB = urlSearchB.split("=")[1];
+        userTrovo = urlSearch[0];
+        userTrovo = userTrovo.split("=")[1];
+        userTwitch = urlSearch[1];
+        userTwitch = userTwitch.split("=")[1];
+        
+        if(userTrovo !== '' && userTwitch !== ''){
+            
+            document.querySelector('#TrovoUser').innerHTML = '<iframe id="chatTrovo" frameborder="0" src="https://trovo.live/chat/'+ userTrovo +'?">'
+            
+            var availableScreenWidth = window.innerWidth;
+            var availableScreenHeight = window.innerHeight - 4;
 
-        var availableScreenWidth = window.innerWidth;
-        var availableScreenHeight = window.innerHeight - 4;
+            var widthjs = availableScreenWidth - 350;
 
-        var widthjs = availableScreenWidth - 350;
+            var embed = new Twitch.Embed("twitch-embed", {
+                width: widthjs,
+                height: availableScreenHeight,
+                channel: userTwitch,
+                layout: "video-with-chat",
+                autoplay: true,
+                parent: ["eternitegik.github.io"],
+                id: "pl"
+            });
 
-        var embed = new Twitch.Embed("twitch-embed", {
-            width: widthjs,
-            height: availableScreenHeight,
-            channel: "shnumi",
-            layout: "video-with-chat",
-            autoplay: true,
-            parent: ["eternitegik.github.io"],
-            id: "pl"
-        });
+            embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
+                var player = embed.getPlayer();
+                player.play();
+            });            
 
-        embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
-            var player = embed.getPlayer();
-            player.play();
-        });            
+            document.getElementById("Twitc").style.width = widthjs + 'px';
+            document.getElementById("Twitc").style.height = availableScreenHeight + 'px';
 
-        document.getElementById("Twitc").style.width = widthjs + 'px';
-        document.getElementById("Twitc").style.height = availableScreenHeight + 'px';
-
-        document.getElementById("chatTrovo").style.height = availableScreenHeight + 'px';
+            document.getElementById("chatTrovo").style.height = availableScreenHeight + 'px';
+            
+            document.getElementById("mess").style.display = 'none';
+            document.getElementById("stream").style.display = 'inherit';
+        }
+        else{
+            document.querySelector('#mess').innerHTML = 'Заполните в URL параметры Trovo= и Twitch= именем стримера с соответствующих площядок и перейдите по ссылке'
+        }
        }
     else{
-        var url = urlProtocol + '//' +  urlHost + urlPathname + "?Trovo=&Tritch="
-        window.open(url);
-        console.log(urlProtocol+ '//' + urlHost + urlPathname + "?Trovo=&Tritch=");
+        var ur = urlProtocol + '//' +  urlHost + urlPathname + "?Trovo=&Twitch="
+        window.open(ur);
+        console.log(ur);
         
         console.log('----------------------3');
+        
+        document.querySelector('#mess').innerHTML = '<a href="' + ur + '">Перейдите по: '+ur+'</a>' 
     }
 }
